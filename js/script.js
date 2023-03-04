7c91523b817a9a34ae95413196d6312ef0696022
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-04 11:37:15 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start');
+    });
+
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log('Move');
+    });
+
+
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('End');
+    });
+
+    
+});
